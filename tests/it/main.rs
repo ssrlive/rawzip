9730427fb4817c703c7d4ab63eec4a09c36462f4
Nick Babcock <nbabcock19@hotmail.com>
2025-05-25 09:42:20 -0500

    Expand zip integration tests to be ran individually

--- tests/it/main.rs
+++ tests/it/main.rs
@@ -3,7 +3,22 @@ use rawzip::{Error, ErrorKind};
 use std::fs::File;
 use std::io::Cursor;
 use std::path::Path;
-use std::sync::LazyLock;
+
+macro_rules! zip_test_case {
+    ($name:expr, $case:expr) => {
+        paste::paste! {
+            #[test]
+            fn [<test_ $name _reader >]() {
+                run_zip_test_case_reader(&$case);
+            }
+
+            #[test]
+            fn [<test_ $name _slice >]() {
+                run_zip_test_case_slice(&$case);
+            }
+        }
+    };
+}
 
 #[derive(Debug, Default)]
 struct ZipTestCase {
@@ -26,228 +41,197 @@ enum ExpectedContent {
     // Size(u64),
 }
 
-static ZIP_TEST_CASES: LazyLock<Vec<ZipTestCase>> = LazyLock::new(|| {
-    vec![
-        ZipTestCase {
-            name: "zip64.zip",
-            comment: Some(b""),
-            files: vec![ZipTestFileEntry {
-                name: "README",
-                expected_content: ExpectedContent::Content(
-                    b"This small file is in ZIP64 format.\n".to_vec(),
-                ),
-            }],
-            ..Default::default()
-        },
-        ZipTestCase {
-            name: "test.zip",
-            comment: Some(b"This is a zipfile comment."),
-            files: vec![
-                ZipTestFileEntry {
-                    name: "test.txt",
-                    expected_content: ExpectedContent::Content(
-                        b"This is a test text file.\n".to_vec(),
-                    ),
-                },
-                ZipTestFileEntry {
-                    name: "gophercolor16x16.png",
-                    expected_content: ExpectedContent::File("gophercolor16x16.png"),
-                },
-            ],
-            ..Default::default()
-        },
-        ZipTestCase {
-            name: "readme.notzip",
-            expected_error_kind: Some(ErrorKind::MissingEndOfCentralDirectory),
-            ..Default::default()
-        },
-        ZipTestCase {
-            name: "test-trailing-junk.zip",
-            comment: Some(b"This is a zipfile comment."),
-            files: vec![
-                ZipTestFileEntry {
-                    name: "test.txt",
-                    expected_content: ExpectedContent::Content(
-                        b"This is a test text file.\n".to_vec(),
-                    ),
-                },
-                ZipTestFileEntry {
-                    name: "gophercolor16x16.png",
-                    expected_content: ExpectedContent::File("gophercolor16x16.png"),
-                },
-            ],
-            ..Default::default()
-        },
-        ZipTestCase {
-            name: "test-prefix.zip",
-            comment: Some(b"This is a zipfile comment."),
-            files: vec![
-                ZipTestFileEntry {
-                    name: "test.txt",
-                    expected_content: ExpectedContent::Content(
-                        b"This is a test text file.\n".to_vec(),
-                    ),
-                },
-                ZipTestFileEntry {
-                    name: "gophercolor16x16.png",
-                    expected_content: ExpectedContent::File("gophercolor16x16.png"),
-                },
-            ],
-            ..Default::default()
-        },
-        ZipTestCase {
-            name: "symlink.zip",
-            files: vec![ZipTestFileEntry {
-                name: "symlink",
-                expected_content: ExpectedContent::Content(b"../target".to_vec()),
-            }],
-            ..Default::default()
-        },
-        ZipTestCase {
-            name: "readme.zip",
-            ..Default::default()
-        },
-        ZipTestCase {
-            // created in windows XP file manager.
-            name: "winxp.zip",
-            files: vec![
-                ZipTestFileEntry {
-                    name: "hello",
-                    expected_content: ExpectedContent::Content(b"world \r\n".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "dir/bar",
-                    expected_content: ExpectedContent::Content(b"foo \r\n".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "dir/empty/",
-                    expected_content: ExpectedContent::Content(b"".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "readonly",
-                    expected_content: ExpectedContent::Content(b"important \r\n".to_vec()),
-                },
-            ],
-            ..Default::default()
-        },
-        ZipTestCase {
-            // created by Zip 3.0 under Linux
-            name: "unix.zip",
-            files: vec![
-                ZipTestFileEntry {
-                    name: "hello",
-                    expected_content: ExpectedContent::Content(b"world \r\n".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "dir/bar",
-                    expected_content: ExpectedContent::Content(b"foo \r\n".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "dir/empty/",
-                    expected_content: ExpectedContent::Content(b"".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "readonly",
-                    expected_content: ExpectedContent::Content(b"important \r\n".to_vec()),
-                },
-            ],
-            ..Default::default()
-        },
-        ZipTestCase {
-            // created by Go, after we wrote the "optional" data
-            // descriptor signatures (which are required by macOS)
-            name: "go-with-datadesc-sig.zip",
-            files: vec![
-                ZipTestFileEntry {
-                    name: "foo.txt",
-                    expected_content: ExpectedContent::Content(b"foo\n".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "bar.txt",
-                    expected_content: ExpectedContent::Content(b"bar\n".to_vec()),
-                },
-            ],
-            ..Default::default()
-        },
-        // ZipTestCase {
-        //     name: "Bad-CRC32-in-data-descriptor",
-        //     files: vec![
-        //         ZipTestFileEntry {
-        //             name: "foo.txt",
-        //             expected_content: ExpectedContent::Content(b"foo\n".to_vec()),
-        //         },
-        //         ZipTestFileEntry {
-        //             name: "bar.txt",
-        //             expected_content: ExpectedContent::Content(b"bar\n".to_vec()),
-        //         },
-        //     ],
-        //     ..Default::default()
-        // },
-        // Tests that we verify (and accept valid) crc32s on files
-        // with crc32s in their file header (not in data descriptors)
-        ZipTestCase {
-            name: "crc32-not-streamed.zip",
-            files: vec![
-                ZipTestFileEntry {
-                    name: "foo.txt",
-                    expected_content: ExpectedContent::Content(b"foo\n".to_vec()),
-                },
-                ZipTestFileEntry {
-                    name: "bar.txt",
-                    expected_content: ExpectedContent::Content(b"bar\n".to_vec()),
-                },
-            ],
-            ..Default::default()
-        },
-        // Tests that we verify (and reject invalid) crc32s on files
-        // with crc32s in their file header (not in data descriptors)
-        // {
-        // 	Name:   "crc32-not-streamed.zip",
-        // 	Source: returnCorruptNotStreamedZip,
-        // 	File: []ZipTestFile{
-        // 		{
-        // 			Name:       "foo.txt",
-        // 			Content:    []byte("foo\n"),
-        // 			Modified:   time.Date(2012, 3, 8, 16, 59, 10, 0, timeZone(-8*time.Hour)),
-        // 			Mode:       0644,
-        // 			ContentErr: ErrChecksum,
-        // 		},
-        // 		{
-        // 			Name:     "bar.txt",
-        // 			Content:  []byte("bar\n"),
-        // 			Modified: time.Date(2012, 3, 8, 16, 59, 12, 0, timeZone(-8*time.Hour)),
-        // 			Mode:     0644,
-        // 		},
-        // 	},
-        // },
+zip_test_case!(
+    "test",
+    ZipTestCase {
+        name: "test.zip",
+        comment: Some(b"This is a zipfile comment."),
+        files: vec![
+            ZipTestFileEntry {
+                name: "test.txt",
+                expected_content: ExpectedContent::Content(b"This is a test text file.\n".to_vec(),),
+            },
+            ZipTestFileEntry {
+                name: "gophercolor16x16.png",
+                expected_content: ExpectedContent::File("gophercolor16x16.png"),
+            },
+        ],
+        ..Default::default()
+    }
+);
 
-        // Another zip64 file with different Extras fields. (golang.org/issue/7069)
-        ZipTestCase {
-            name: "zip64-2.zip",
-            files: vec![ZipTestFileEntry {
-                name: "README",
-                expected_content: ExpectedContent::Content(
-                    b"This small file is in ZIP64 format.\n".to_vec(),
-                ),
-            }],
-            ..Default::default()
-        },
-        // Largest possible non-zip64 file, with no zip64 header.
-        // {
-        // 	Name:   "big.zip",
-        // 	Source: returnBigZipBytes,
-        // 	File: []ZipTestFile{
-        // 		{
-        // 			Name:     "big.file",
-        // 			Content:  nil,
-        // 			Size:     1<<32 - 1,
-        // 			Modified: time.Date(1979, 11, 30, 0, 0, 0, 0, time.UTC),
-        // 			Mode:     0666,
-        // 		},
-        // 	},
-        // },
-    ]
-});
+zip_test_case!(
+    "readme_notzip",
+    ZipTestCase {
+        name: "readme.notzip",
+        expected_error_kind: Some(ErrorKind::MissingEndOfCentralDirectory),
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "test_trailing_junk",
+    ZipTestCase {
+        name: "test-trailing-junk.zip",
+        comment: Some(b"This is a zipfile comment."),
+        files: vec![
+            ZipTestFileEntry {
+                name: "test.txt",
+                expected_content: ExpectedContent::Content(b"This is a test text file.\n".to_vec(),),
+            },
+            ZipTestFileEntry {
+                name: "gophercolor16x16.png",
+                expected_content: ExpectedContent::File("gophercolor16x16.png"),
+            },
+        ],
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "test_prefix",
+    ZipTestCase {
+        name: "test-prefix.zip",
+        comment: Some(b"This is a zipfile comment."),
+        files: vec![
+            ZipTestFileEntry {
+                name: "test.txt",
+                expected_content: ExpectedContent::Content(b"This is a test text file.\n".to_vec(),),
+            },
+            ZipTestFileEntry {
+                name: "gophercolor16x16.png",
+                expected_content: ExpectedContent::File("gophercolor16x16.png"),
+            },
+        ],
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "symlink",
+    ZipTestCase {
+        name: "symlink.zip",
+        files: vec![ZipTestFileEntry {
+            name: "symlink",
+            expected_content: ExpectedContent::Content(b"../target".to_vec()),
+        }],
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "readme",
+    ZipTestCase {
+        name: "readme.zip",
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "winxp",
+    ZipTestCase {
+        // created in windows XP file manager.
+        name: "winxp.zip",
+        files: vec![
+            ZipTestFileEntry {
+                name: "hello",
+                expected_content: ExpectedContent::Content(b"world \r\n".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "dir/bar",
+                expected_content: ExpectedContent::Content(b"foo \r\n".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "dir/empty/",
+                expected_content: ExpectedContent::Content(b"".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "readonly",
+                expected_content: ExpectedContent::Content(b"important \r\n".to_vec()),
+            },
+        ],
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "unix",
+    ZipTestCase {
+        // created by Zip 3.0 under Linux
+        name: "unix.zip",
+        files: vec![
+            ZipTestFileEntry {
+                name: "hello",
+                expected_content: ExpectedContent::Content(b"world \r\n".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "dir/bar",
+                expected_content: ExpectedContent::Content(b"foo \r\n".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "dir/empty/",
+                expected_content: ExpectedContent::Content(b"".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "readonly",
+                expected_content: ExpectedContent::Content(b"important \r\n".to_vec()),
+            },
+        ],
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "go_with_datadesc_sig",
+    ZipTestCase {
+        // created by Go, after we wrote the "optional" data
+        // descriptor signatures (which are required by macOS)
+        name: "go-with-datadesc-sig.zip",
+        files: vec![
+            ZipTestFileEntry {
+                name: "foo.txt",
+                expected_content: ExpectedContent::Content(b"foo\n".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "bar.txt",
+                expected_content: ExpectedContent::Content(b"bar\n".to_vec()),
+            },
+        ],
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "crc32_not_streamed",
+    ZipTestCase {
+        name: "crc32-not-streamed.zip",
+        files: vec![
+            ZipTestFileEntry {
+                name: "foo.txt",
+                expected_content: ExpectedContent::Content(b"foo\n".to_vec()),
+            },
+            ZipTestFileEntry {
+                name: "bar.txt",
+                expected_content: ExpectedContent::Content(b"bar\n".to_vec()),
+            },
+        ],
+        ..Default::default()
+    }
+);
+
+zip_test_case!(
+    "zip64_2",
+    ZipTestCase {
+        name: "zip64-2.zip",
+        files: vec![ZipTestFileEntry {
+            name: "README",
+            expected_content: ExpectedContent::Content(
+                b"This small file is in ZIP64 format.\n".to_vec(),
+            ),
+        }],
+        ..Default::default()
+    }
+);
 
 fn process_archive_files<R: rawzip::ReaderAt>(
     archive: &rawzip::ZipArchive<R>,
@@ -276,15 +260,9 @@ fn process_archive_files<R: rawzip::ReaderAt>(
                     if file_name == expected_file.name {
                         actual_files_found += 1;
                         found_file = true;
-                        println!("Found file: {} in {}", expected_file.name, case.name);
 
                         let position = entry.wayfinder();
-                        let ent = archive.get_entry(position).inspect_err(|e| {
-                            println!(
-                                "Failed to get_entry for {} in {}: {:?}",
-                                expected_file.name, case.name, e
-                            )
-                        })?;
+                        let ent = archive.get_entry(position)?;
 
                         let mut data = Vec::new();
                         match entry.compression_method() {
@@ -373,16 +351,10 @@ fn process_slice_archive_files(
                     if file_name == expected_file.name {
                         actual_files_found += 1;
                         found_file = true;
-                        println!("Found file: {} in {}", expected_file.name, case.name);
 
                         let position = entry.wayfinder();
 
-                        let ent = archive.get_entry(position).inspect_err(|e| {
-                            println!(
-                                "Failed to get_entry for {} in {}: {:?}",
-                                expected_file.name, case.name, e
-                            )
-                        })?;
+                        let ent = archive.get_entry(position)?;
 
                         let mut data = Vec::new();
                         match entry.compression_method() {
@@ -445,10 +417,8 @@ fn process_slice_archive_files(
     Ok(())
 }
 
-fn run_zip_test_case(case: &ZipTestCase) {
-    println!("Running test case: {}", case.name);
-
-    let file_path = Path::new("assets").join(case.name);
+fn run_zip_test_case_reader(case: &ZipTestCase) {
+    let file_path = Path::new("assets").join(&case.name);
     let f = File::open(file_path).unwrap();
 
     fn processor(f: File, case: &ZipTestCase) -> Result<(), Error> {
@@ -482,9 +452,8 @@ fn run_zip_test_case(case: &ZipTestCase) {
 }
 
 fn run_zip_test_case_slice(case: &ZipTestCase) {
-    println!("Running test case: {}", case.name);
     fn processor(case: &ZipTestCase) -> Result<(), Error> {
-        let file_path = Path::new("assets").join(case.name);
+        let file_path = Path::new("assets").join(&case.name);
         let data = std::fs::read(file_path).unwrap();
 
         let archive = rawzip::ZipArchive::from_slice(data.as_slice())?;
@@ -516,7 +485,6 @@ fn run_zip_test_case_slice(case: &ZipTestCase) {
 }
 
 fn errors_eq(a: &Error, b: &ErrorKind) -> bool {
-    println!("Comparing errors: {:?} vs {:?}", a, b);
     match (a.kind(), b) {
         (
             ErrorKind::InvalidSignature {
@@ -553,20 +521,6 @@ fn errors_eq(a: &Error, b: &ErrorKind) -> bool {
         ) => true,
         (ErrorKind::BufferTooSmall, ErrorKind::BufferTooSmall) => true,
         _ => false,
-    }
-}
-
-#[test]
-fn run_all_zip_tests() {
-    for case in ZIP_TEST_CASES.iter() {
-        run_zip_test_case(case);
-    }
-}
-
-#[test]
-fn run_all_zip_tests_slice() {
-    for case in ZIP_TEST_CASES.iter() {
-        run_zip_test_case_slice(case);
     }
 }
 
